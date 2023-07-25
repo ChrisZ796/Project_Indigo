0ed6b3ea4835ce81ab800dbd7d96b052223ee5d9
ChrisZ-360 <chrisz.wshs@gmail.com>
2023-07-25 10:11:36 -0400

    more comments

--- gameScript.js
+++ gameScript.js
@@ -1,3 +1,4 @@
+// declaring and initializing variable for stat
 let startButton = document.getElementById("start");
 let statPage = document.getElementById("stats");
 let distance = 0;
@@ -11,40 +12,40 @@ let attemptsStat = document.getElementById("attempts")
 distanceStat.textContent = distance;
 coinsStat.textContent = coins;
 attemptsStat.textContent = attempts;
-
+// event listener for the start button to go to the game mode
 startButton.addEventListener("click", hideElements, false);
 statPage.addEventListener("click", openStats, false);
-
+// event listener for the stat page to go back to main
 document.getElementById("statHeader").addEventListener("click", backToMain, false);
-
+// event listener for going to the credit page
 let creditsButton = document.getElementById("credits");
 
 creditsButton.addEventListener("click", hideElements, false);
 creditsButton.addEventListener("click", showCredits, false);
-
+// event listener for going to the setting page or heading back from the setting page.
 document.getElementById("settings").addEventListener("click", openSettings, false);
 
 document.getElementById("title").addEventListener("click", hideSettings, false);
-
+// this hide the elements on the page
 function hideElements()
 {
     document.getElementById("mainContainer").style.display = "none";
 }
-
+// show the page of stats
 function openStats()
 {
     document.getElementById("mainContainer").style.display = "none";
     document.getElementById("statContainer").style.display = "block";
     dropInElementbyID("statContainer");
 }
-
+// head back to main from different pages
 function backToMain()
 {
     document.getElementById("mainContainer").style.display = "block";
     document.getElementById("statContainer").style.display = "none";
     document.getElementById("Credits").style.display = "none";
 }
-
+// show the credit page
 function showCredits()
 {
     let creditsPage = document.getElementById("Credits");
@@ -52,14 +53,14 @@ function showCredits()
     dropInElementbyID("Credits");
     document.getElementById("creditsHeader").addEventListener("click", backToMain, false);
 }
-
+// show the setting page
 function openSettings()
 {
     document.getElementById("settingsBar").style.display = "grid";
     dropInElementbyID("settingsBar");
     hideElements();
 }
-
+// show the setting page
 function hideSettings()
 {
     document.getElementById("settingsBar").style.display = "none";
